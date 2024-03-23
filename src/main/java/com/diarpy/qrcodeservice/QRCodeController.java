b4e8612b50135e9f436ddba786a88341a01921d0
Rutzno <d.rutzno@gmail.com>
2024-03-23 14:14:38 +0000

    stage 2

--- src/main/java/com/diarpy/qrcodeservice/QRCodeController.java
+++ src/main/java/com/diarpy/qrcodeservice/QRCodeController.java
@@ -1,11 +1,18 @@
 package com.diarpy.qrcodeservice;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 /**
  * @author Mack_TB
@@ -24,9 +31,23 @@ public class QRCodeController {
     }
 
     // used by clients to retrieve QR code images
+    // used by clients to retrieve QR code images
     @GetMapping("/qrcode")
-    @ResponseStatus(code = HttpStatus.NOT_IMPLEMENTED)
-    public void getQrcode() {
+    public ResponseEntity<byte[]> getImage() throws IOException {
+        BufferedImage bufferedImage = new BufferedImage(250, 250, BufferedImage.TYPE_INT_RGB);
+        Graphics2D g = bufferedImage.createGraphics();
+        g.setColor(Color.WHITE);
+        g.fillRect(0, 0, 250, 250);
 
+        try(var baos = new ByteArrayOutputStream()) {
+            ImageIO.write(bufferedImage, "png", baos);
+            byte[] bytes = baos.toByteArray();
+            return ResponseEntity
+                    .ok()
+                    .contentType(MediaType.IMAGE_PNG)
+                    .body(bytes);
+        } /*catch (IOException e) {
+            e.printStackTrace();
+        }*/
     }
 }
