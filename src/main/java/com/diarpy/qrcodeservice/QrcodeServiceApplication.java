ed65c67e625a8d0a06d0db64130ed0fa607e1c31
Rutzno <d.rutzno@gmail.com>
2024-03-23 14:00:04 +0000

    first commit

--- src/main/java/com/diarpy/qrcodeservice/QrcodeServiceApplication.java
+++ src/main/java/com/diarpy/qrcodeservice/QrcodeServiceApplication.java
@@ -3,6 +3,12 @@ package com.diarpy.qrcodeservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Mack_TB
+ * @since 20/03/2024
+ * @version 1.0.0
+ */
+
 @SpringBootApplication
 public class QrcodeServiceApplication {
 
