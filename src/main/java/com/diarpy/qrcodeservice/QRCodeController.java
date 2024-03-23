2ad249fbf34d20eee8b0e488dbb1d64aeec131a7
Rutzno <d.rutzno@gmail.com>
2024-03-23 14:20:48 +0000

    stage 3

--- src/main/java/com/diarpy/qrcodeservice/QRCodeController.java
+++ src/main/java/com/diarpy/qrcodeservice/QRCodeController.java
@@ -1,26 +1,26 @@
 package com.diarpy.qrcodeservice;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.imageio.ImageIO;
+import javax.print.attribute.standard.Media;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-
-/**
- * @author Mack_TB
- * @since 20/03/2024
- * @version 1.0.2
- */
+import java.util.Map;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/api")
 public class QRCodeController {
+    final Set<String> SUPPORTED_TYPES = Set.of("png", "jpeg", "gif");
+    final Map<String, MediaType> MEDIATYPES = Map.of("png", MediaType.IMAGE_PNG,
+            "jpeg", MediaType.IMAGE_JPEG,
+            "gif", MediaType.IMAGE_GIF);
 
     // allows clients to ping the service, verifying its operation and availability.
     @GetMapping("/health")
@@ -30,21 +30,31 @@ public class QRCodeController {
 
     // used by clients to retrieve QR code images
     @GetMapping("/qrcode")
-    public ResponseEntity<byte[]> getImage() throws IOException {
-        BufferedImage bufferedImage = new BufferedImage(250, 250, BufferedImage.TYPE_INT_RGB);
-        Graphics2D g = bufferedImage.createGraphics();
-        g.setColor(Color.WHITE);
-        g.fillRect(0, 0, 250, 250);
+    public ResponseEntity<Object> getImage(@RequestParam int size, @RequestParam String type) throws IOException {
+        if (size < 150 || size > 350) {
+            return ResponseEntity.badRequest()
+                    .body(Map.of("error", "Image size must be between 150 and 350 pixels"));
+        } else {
+            if (!SUPPORTED_TYPES.contains(type)) {
+                return ResponseEntity.badRequest()
+                        .body(Map.of("error", "Only png, jpeg and gif image types are supported"));
+            } else {
+                BufferedImage bufferedImage = new BufferedImage(size, size, BufferedImage.TYPE_INT_RGB);
+                Graphics2D g = bufferedImage.createGraphics();
+                g.setColor(Color.WHITE);
+                g.fillRect(0, 0, size, size);
 
-        try(var baos = new ByteArrayOutputStream()) {
-            ImageIO.write(bufferedImage, "png", baos);
-            byte[] bytes = baos.toByteArray();
-            return ResponseEntity
-                    .ok()
-                    .contentType(MediaType.IMAGE_PNG)
-                    .body(bytes);
-        } /*catch (IOException e) {
-            e.printStackTrace();
-        }*/
+                try (var baos = new ByteArrayOutputStream()) {
+                    ImageIO.write(bufferedImage, type, baos);
+                    byte[] bytes = baos.toByteArray();
+                    return ResponseEntity
+                            .ok()
+                            .contentType(MEDIATYPES.get(type))
+                            .body(bytes);
+                } /*catch (IOException e) {
+                    e.printStackTrace();
+                }*/
+            }
+        }
     }
 }
