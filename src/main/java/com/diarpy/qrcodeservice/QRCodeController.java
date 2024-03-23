1fbcd606c670d2b2e064627bfba58b7b711d13b6
Rutzno <d.rutzno@gmail.com>
2024-03-23 14:16:55 +0000

    stage 2

--- src/main/java/com/diarpy/qrcodeservice/QRCodeController.java
+++ src/main/java/com/diarpy/qrcodeservice/QRCodeController.java
@@ -1,11 +1,9 @@
 package com.diarpy.qrcodeservice;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.imageio.ImageIO;
