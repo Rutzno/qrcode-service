ed174c5c1f61512d41631809b6c907abee953bd7
Rutzno <d.rutzno@gmail.com>
2024-03-23 14:40:05 +0000

    stage 5

--- src/main/java/com/diarpy/qrcodeservice/QRCodeController.java
+++ src/main/java/com/diarpy/qrcodeservice/QRCodeController.java
@@ -1,36 +1,32 @@
 package com.diarpy.qrcodeservice;
-
 import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
 import com.google.zxing.WriterException;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.imageio.ImageIO;
-import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.Map;
 import java.util.Set;
 
-/**
- * @author Mack_TB
- * @since 20/03/2024
- * @version 1.0.4
- */
-
 @RestController
 @RequestMapping("/api")
 public class QRCodeController {
-    private final Set<String> SUPPORTED_TYPES = Set.of("png", "jpeg", "gif");
-    private final Map<String, MediaType> MEDIATYPES = Map.of("png", MediaType.IMAGE_PNG,
+    final Set<Character> SUPPORTED_CORRECTION_LEVELS = Set.of('L', 'M', 'Q', 'H');
+    final Map<Character, ErrorCorrectionLevel> CORRECTION_LEVELS = Map.of('L', ErrorCorrectionLevel.L,
+            'M', ErrorCorrectionLevel.M,
+            'Q', ErrorCorrectionLevel.Q,
+            'H', ErrorCorrectionLevel.H);
+    final Set<String> SUPPORTED_TYPES = Set.of("png", "jpeg", "gif");
+    final Map<String, MediaType> MEDIATYPES = Map.of("png", MediaType.IMAGE_PNG,
             "jpeg", MediaType.IMAGE_JPEG,
             "gif", MediaType.IMAGE_GIF);
 
@@ -42,7 +38,10 @@ public class QRCodeController {
 
     // used by clients to retrieve QR code images
     @GetMapping("/qrcode")
-    public ResponseEntity<Object> getImage(@RequestParam String contents, @RequestParam int size, @RequestParam String type) throws IOException {
+    public ResponseEntity<Object> getImage(@RequestParam String contents,
+                                           @RequestParam(defaultValue = "250") int size,
+                                           @RequestParam(defaultValue = "L") char correction,
+                                           @RequestParam(defaultValue = "png") String type) throws IOException {
         if (contents.isBlank()) {
             return ResponseEntity.badRequest()
                     .body(Map.of("error", "Contents cannot be null or blank"));
@@ -50,31 +49,33 @@ public class QRCodeController {
         if (size < 150 || size > 350) {
             return ResponseEntity.badRequest()
                     .body(Map.of("error", "Image size must be between 150 and 350 pixels"));
-        } else {
-            if (!SUPPORTED_TYPES.contains(type)) {
-                return ResponseEntity.badRequest()
-                        .body(Map.of("error", "Only png, jpeg and gif image types are supported"));
-            } else {
+        } if (!SUPPORTED_CORRECTION_LEVELS.contains(correction)) {
+            return ResponseEntity.badRequest()
+                    .body(Map.of("error", "Permitted error correction levels are L, M, Q, H"));
+        }
+        if (!SUPPORTED_TYPES.contains(type)) {
+            return ResponseEntity.badRequest()
+                    .body(Map.of("error", "Only png, jpeg and gif image types are supported"));
+        }
                /* BufferedImage bufferedImage = new BufferedImage(size, size, BufferedImage.TYPE_INT_RGB);
                 Graphics2D g = bufferedImage.createGraphics();
                 g.setColor(Color.WHITE);
                 g.fillRect(0, 0, size, size);*/
 
-                QRCodeWriter writer = new QRCodeWriter();
-                try (var baos = new ByteArrayOutputStream()) {
-                    BitMatrix bitMatrix = writer.encode(contents, BarcodeFormat.QR_CODE, size, size);
-                    BufferedImage bufferedImage = MatrixToImageWriter.toBufferedImage(bitMatrix);
+        QRCodeWriter writer = new QRCodeWriter();
+        try (var baos = new ByteArrayOutputStream()) {
+            Map<EncodeHintType, ?> hints = Map.of(EncodeHintType.ERROR_CORRECTION, CORRECTION_LEVELS.get(correction));
+            BitMatrix bitMatrix = writer.encode(contents, BarcodeFormat.QR_CODE, size, size, hints);
+            BufferedImage bufferedImage = MatrixToImageWriter.toBufferedImage(bitMatrix);
 
-                    ImageIO.write(bufferedImage, type, baos);
-                    byte[] bytes = baos.toByteArray();
-                    return ResponseEntity
-                            .ok()
-                            .contentType(MEDIATYPES.get(type))
-                            .body(bytes);
-                } catch (WriterException e) {
-                    throw new RuntimeException(e);
-                }
-            }
+            ImageIO.write(bufferedImage, type, baos);
+            byte[] bytes = baos.toByteArray();
+            return ResponseEntity
+                    .ok()
+                    .contentType(MEDIATYPES.get(type))
+                    .body(bytes);
+        } catch (WriterException e) {
+            throw new RuntimeException(e);
         }
     }
 }
